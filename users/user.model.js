ec1c7f01281ffb97fc3d0f49279a7c108d444cab
Mr Mendes <mendes.paul28@gmail.com>
2022-01-28 14:38:00 +0100

    BeerTech node msql test

---
+++ users/user.model.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const attributes = {
+        name: { type: DataTypes.STRING, allowNull: false },
+        email: { type: DataTypes.STRING, allowNull: false },
+        phoneNumber: { type: DataTypes.STRING, allowNull: false },
+        hash: { type: DataTypes.STRING, allowNull: false },
+        passport: { type: DataTypes.STRING, allowNull: false }
+    };
+
+    const options = {
+        defaultScope: {
+            // exclude hash by default
+            attributes: { exclude: ['hash'] }
+        },
+        scopes: {
+            // include hash with this scope
+            withHash: { attributes: {}, }
+        }
+    };
+
+    return sequelize.define('User', attributes, options);
+}
